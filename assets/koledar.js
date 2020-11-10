eebb5e1be00c1d1dc25f59c17cde88a4b3231c39
RoKKim <rokmiklavcic9@gmail.com>
2020-11-10 09:49:14 +0100

    Dodani dnei tedna, nedelje, danasnji datum

--- assets/koledar.js
+++ assets/koledar.js
@@ -1,9 +1,17 @@
 let Koledar = function(options) {
     const init = function() {
-        options.danasnjiDatum = new Date();
+        options.danasnjiDatum = novDatum();
         options.mesec_label = options.container.querySelector('.koledar_header_label');
 
+        nastaviKoledarHeader();
         ustvariMesec();
+    }
+
+    const novDatum = function() {
+        let datum = new Date;
+        // ker nas cas ne zanima, ga nastavimo na 0
+        datum.setHours(0, 0, 0, 0)
+        return datum;
     }
 
     const ustvariDan = function(datum) {
@@ -12,10 +20,19 @@ let Koledar = function(options) {
         datumSpan.innerHTML = datum.getDate();
         datumDiv.className = 'datum';
 
+        // ce je datum nedelja, mu nastavimo poseben class
+        if (datum.getDay() === 0) {
+            console.log('aaa')
+            datumDiv.classList.add('datum_nedelja');
+        }
         // prvemu datumu v mesecu dodamo odmik, da ga poravnamo z dnevom v tednu
         if (datum.getDate() === 1) {
             // ker getDay() vrne nedeljo kot 0, jo z ali operatorjem nadomestimo z 7
             datumDiv.style.marginLeft = ((datum.getDay() || 7 - 1) * 100 / 7) + '%';
+        }
+        // ce je datum enak danasnjemu, mu nastavimo poseben class
+        if (datum.getTime() === options.danasnjiDatum.getTime()) {
+            datumDiv.classList.add('datum_danes');
         }
 
         datumDiv.appendChild(datumSpan);
@@ -25,7 +42,7 @@ let Koledar = function(options) {
     const ustvariMesec = function() {
         options.mesec_label.innerHTML = options.meseci[options.danasnjiDatum.getMonth()] + ' ' + options.danasnjiDatum.getFullYear();
 
-        let datum = new Date();
+        let datum = novDatum();
         // dneve izpisujemo od prvega dneva meseca dalje
         datum.setDate(1);
 
@@ -35,6 +52,15 @@ let Koledar = function(options) {
             ustvariDan(datum);
             datum.setDate(datum.getDate() + 1);
         }
+    }
+
+    const nastaviKoledarHeader = function() {
+        options.dnevi.forEach((dan) => {
+            let danSpan = document.createElement('span');
+            danSpan.innerHTML = dan;
+
+            options.container.querySelector('.koledar_teden').appendChild(danSpan);
+        })
     }
 
     init();
