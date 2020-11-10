87c113a1a3394939811d14d4701fef01485ed970
RoKKim <rokmiklavcic9@gmail.com>
2020-11-10 13:14:32 +0100

    Branje datoteke

--- assets/koledar.js
+++ assets/koledar.js
@@ -1,8 +1,9 @@
 let Koledar = function(options) {
+    let prazniki = [];
+
     const init = function() {
         options.danasnjiDatum = novDatum();
         options.datum = novDatum();
-        options.mesecLabel = options.container.querySelector('.koledar_header_label');
 
         options.container.querySelector('#prejsni_mesec').addEventListener("click", function(){
             options.datum.setMonth(options.datum.getMonth() - 1);
@@ -12,25 +13,58 @@ let Koledar = function(options) {
             options.datum.setMonth(options.datum.getMonth() + 1);
             ustvariMesec();
         });
-        options.container.querySelector('#mesec_select').addEventListener("change", function(){
-            ustvariMesec();
+        options.container.querySelector('#zapri_koledar').addEventListener("click", function(){
+            options.container.querySelector('.koledar_body').style.display = "none";
         });
+        options.container.querySelector('#file_selector').addEventListener("change", (event) => {
+            preberiDatoteko(event.target.files[0], function(zapisi) {
+                zapisi.forEach((zapis) => {
+                    let praznik = {};
+                    zapis = zapis.split(';');
+                    praznik.datum = zapis[0].split('.');
+                    praznik.datum  = new Date(praznik.datum[2], praznik.datum[1] - 1, praznik.datum[0]);
+                    // trim da se znebimo whitespace karakterjev
+                    praznik.ponavljajoc = zapis[1].trim() === 'X';
+
+                    prazniki.push(praznik);
+                })
+
+                ustvariMesec();
+            });
+        });
+
         options.container.querySelector('#mesec_select').addEventListener("change", function(){
             options.datum.setMonth(options.meseci.indexOf(this.value));
             ustvariMesec();
+            options.container.querySelector('.koledar_body').style.display = "block";
         });
         options.container.querySelector('#leto_input').addEventListener("change", function(){
             options.datum.setFullYear(this.value);
             ustvariMesec();
+            options.container.querySelector('.koledar_body').style.display = "block";
         });
         options.container.querySelector('#datum_input').addEventListener("change", function(){
             options.datum = new Date(this.value.split('.')[2], this.value.split('.')[1] - 1, this.value.split('.')[0]);
             ustvariMesec();
+            options.container.querySelector('.koledar_body').style.display = "block";
         });
 
         nastaviIzbiroMeseca();
-        nastaviKoledarHeader();
-        ustvariMesec(options.datum.getMonth());
+        nastaviDneveTedna();
+        ustvariMesec();
+    }
+
+    function preberiDatoteko(datoteka, callback) {
+        let reader = new FileReader();
+        reader.readAsText(datoteka);
+
+        reader.onload = function() {
+            callback(reader.result.split('\n'));
+        };
+
+        reader.onerror = function() {
+            console.log(reader.error);
+        };
     }
 
     const nastaviIzbiroMeseca = function() {
@@ -40,6 +74,15 @@ let Koledar = function(options) {
 
             options.container.querySelector('#mesec_select').appendChild(mesecOption);
         })
+    }
+
+    const nastaviDneveTedna = function() {
+        options.dnevi.forEach((dan) => {
+            let danSpan = document.createElement('span');
+            danSpan.textContent = dan;
+
+            options.container.querySelector('.koledar_teden').appendChild(danSpan);
+        });
     }
 
     const novDatum = function() {
@@ -68,6 +111,17 @@ let Koledar = function(options) {
         if (datum.getTime() === options.danasnjiDatum.getTime()) {
             datumDiv.classList.add('datum_danes');
         }
+        // preverimo, ce je datum enak kateremu koli od datumov
+        prazniki.forEach((praznik) => {
+            // ce se praznik ponavlja vsako leto, preverimo samo dan in mesec
+            if (praznik.ponavljajoc) {
+                if (datum.getDate() === praznik.datum.getDate() && datum.getMonth() === praznik.datum.getMonth()) {
+                    datumDiv.classList.add('datum_praznik');
+                }
+            } else if (datum.getTime() === praznik.datum.getTime()) {
+                datumDiv.classList.add('datum_praznik');
+            }
+        })
 
         datumDiv.appendChild(datumSpan);
         options.container.querySelector('.koledar_datumi').appendChild(datumDiv);
@@ -77,24 +131,15 @@ let Koledar = function(options) {
         // preden ustvarimo dneve meseca, je potrebno resetirati trenutne
         options.container.querySelector('.koledar_datumi').innerHTML = '';
 
-        options.mesecLabel.textContent = options.meseci[options.datum.getMonth()] + ' ' + options.datum.getFullYear();
-
         // dneve izpisujemo od prvega dneva meseca dalje
         options.datum.setDate(1);
-
         // zanka gre cez vse dneve v mesecu
         for (let i = new Date(options.datum.getTime()); i.getMonth() === options.datum.getMonth(); i.setDate(i.getDate() + 1)) {
             ustvariDan(i);
         }
-    }
 
-    const nastaviKoledarHeader = function() {
-        options.dnevi.forEach((dan) => {
-            let danSpan = document.createElement('span');
-            danSpan.textContent = dan;
-
-            options.container.querySelector('.koledar_teden').appendChild(danSpan);
-        });
+        // nastavimo se label v headerju
+        options.container.querySelector('.koledar_header_label').textContent = options.meseci[options.datum.getMonth()] +' ' + options.datum.getFullYear();
     }
 
     // todo remove listeners when calendar closed
